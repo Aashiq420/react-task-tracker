803550a34514ee57986fe7f5e43af73f68323e09
Aashiq420 <adams.aashiq@gmail.com>
2022-02-09 19:19:03 +0200

    Created components (Button, Header, Tasks, Task)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
+// import React from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from "react"
 
-function App() {
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'one piece rulez',
+        day: 'Friday',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'one piece rulez over all',
+        day: 'Sunday',
+        reminder: false
+    }
+])
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
-    <div className="App">
-      <h1>Hello From Aashiq</h1>
+    <div className="container">
+      <Header />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No Tasks To Show'}
     </div>
   );
 }
+
+// USING A CLASS:
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello from a class</h1>
+//   }
+// }
 
 export default App;
